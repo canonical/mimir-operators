499133be19b335ca4f6a67745d350ef9ff4d9072
Luca Bello <luca.bello@canonical.com>
2026-06-18 13:32:40 +0200

    only fetch prometheus_scrape in coordinator

--- coordinator/lib/charms/grafana_k8s/v0/grafana_dashboard.py
+++ coordinator/lib/charms/grafana_k8s/v0/grafana_dashboard.py
@@ -217,7 +217,7 @@ LIBAPI = 0
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
 
-LIBPATCH = 49
+LIBPATCH = 48
 
 PYDEPS = ["cosl >= 0.0.50"]
 
@@ -1013,7 +1013,7 @@ class CharmedDashboard:
 
         dashboard_templates = {}
 
-        for path in filter(_is_dashboard, Path(dashboards_path).glob("**/*")):
+        for path in filter(_is_dashboard, Path(dashboards_path).glob("*")):
             try:
                 dashboard_dict = json.loads(path.read_bytes())
             except json.JSONDecodeError as e:
